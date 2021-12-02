45c6dc8382614b97b62c273ed0afca081c8ef0c9
yarovojigor1988 <47006084+yarovojigor1988@users.noreply.github.com>
2021-12-02 23:10:54 +0200

    mongodb

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 const express = require('express')
 const path = require('path')
+const mongoose = require('mongoose')
 const exphbs = require('express-handlebars')
 const homeRoutes = require('./routes/home')
 const cardRoutes = require('./routes/card')
 const addRoutes = require('./routes/add')
+const ordersRoutes = require('./routes/orders')
 const coursesRoutes = require('./routes/courses')
+const User = require('./models/user')
 
 const app = express()
 
@@ -17,6 +20,16 @@ app.engine('hbs', hbs.engine)
 app.set('view engine', 'hbs')
 app.set('views', 'views')
 
+app.use(async (req, res, next) => {
+  try {
+    const user = await User.findById('5cc1d29dcedab01481e03660')
+    req.user = user
+    next()
+  } catch (e) {
+    console.log(e)
+  }
+})
+
 app.use(express.static(path.join(__dirname, 'public')))
 app.use(express.urlencoded({extended: true}))
 
@@ -24,9 +37,34 @@ app.use('/', homeRoutes)
 app.use('/add', addRoutes)
 app.use('/courses', coursesRoutes)
 app.use('/card', cardRoutes)
+app.use('/orders', ordersRoutes)
 
 const PORT = process.env.PORT || 3000
 
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`)
-})
+async function start() {
+  try {
+    const url = `mongodb+srv://vladilen:0I5GEL9uLUcR38GC@cluster0-3rrau.mongodb.net/shop`
+    await mongoose.connect(url, {
+      useNewUrlParser: true,
+      useFindAndModify: false
+    })
+    const candidate = await User.findOne()
+    if (!candidate) {
+      const user = new User({
+        email: 'vladilen@mail.ru',
+        name: 'Vladilen',
+        cart: {items: []}
+      })
+      await user.save()
+    }
+    app.listen(PORT, () => {
+      console.log(`Server is running on port ${PORT}`)
+    })
+  } catch (e) {
+    console.log(e)
+  }
+}
+
+start()
+
+
